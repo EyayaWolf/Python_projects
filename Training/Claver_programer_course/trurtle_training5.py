0b8188e86d39f73b0a75dacb0299486fcfec2080
EyayaWolf <nemethgsz@gmail.com>
2020-07-09 18:46:52 +0200

    Create trurtle_training5.py

---
+++ Training/Claver_programer_course/trurtle_training5.py
@@ -0,0 +1,24 @@
+import turtle
+
+ninja = turtle.Turtle()
+ninja.pencolor("red")
+ninja.speed(0)
+
+def square(length, angle, corner):
+    for i in range(corner):
+        ninja.forward(length)
+        ninja.right(angle)
+    for j in range(corner):
+        ninja.forward(length)
+        ninja.left(angle)
+    for k in range(corner):
+        ninja.backward(length)
+        ninja.left(angle)
+    for l in range(corner):
+        ninja.backward(length)
+        ninja.right(angle)
+
+for i in range(45):
+    square(150, 90, 4)
+    ninja.right(4)
+turtle.done()
